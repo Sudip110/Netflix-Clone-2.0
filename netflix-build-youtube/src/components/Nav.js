5cd89b8f0b544ab4dd372591337f4686f70b5220
Bikahc043 <bikashc043@gmail.com>
2024-02-24 21:12:54 +0530

    added logo

--- netflix-build-youtube/src/components/Nav.js
+++ netflix-build-youtube/src/components/Nav.js
@@ -3,7 +3,10 @@ import React from 'react'
 const Nav = () => {
   return (
     <div className='Nav'>
-        <h1>This is the nav</h1>
+      <img className="nav_logo"
+      src="https://i.pcmag.com/imagery/reviews/05cItXL96l4LE9n02WfDR0h-5.fit_scale.size_760x427.v1582751026.png" alt="" />
+      <img className='nav_avatar'
+      src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117" alt="" />
     </div>
   )
 }

20b1a6bd1af09951417f924eb8515a9ae8ac040b
Bikahc043 <bikashc043@gmail.com>
2024-02-24 20:28:12 +0530

    added firebase

---
+++ netflix-build-youtube/src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='Nav'>
+        <h1>This is the nav</h1>
+    </div>
+  )
+}
+
+export default Nav
---
+++ netflix-build-youtube/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDknc-mwVAB1BX-h2I13XVFu6g6nge7JdQ",
+    authDomain: "netflix-clone-d27a4.firebaseapp.com",
+    projectId: "netflix-clone-d27a4",
+    storageBucket: "netflix-clone-d27a4.appspot.com",
+    messagingSenderId: "263203023223",
+    appId: "1:263203023223:web:08744ed0d779926a841972"
+  };

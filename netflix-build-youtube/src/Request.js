0123e297c9628d9ef90afbfc016d30b26b4653f8
Bikahc043 <bikashc043@gmail.com>
2024-02-25 17:40:29 +0530

    updated banner and added api

---
+++ netflix-build-youtube/src/Request.js
@@ -0,0 +1,5 @@
+const API_KEY ="3fc38a195d79ee230ad3d80cbccc7408"
+
+const requests={
+
+}
--- netflix-build-youtube/src/components/Banner.js
+++ netflix-build-youtube/src/components/Banner.js
@@ -2,22 +2,27 @@ import React from 'react'
 import './Banner.css';
 
 export const Banner = () => {
-  return (
+    function truncate(string,n){
+        return string?.length > n ? string.substr(0,n-1)+'...':string;
+    
+    }
+    return (
     <header className='banner'
         style={{
             backgroundSize:"cover",
-            backgroundImage:`url("https://i.imgur.com/e1hLQ2m.png")`,
+            backgroundImage:`url("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/800px-Black_flag.svg.png")`,
             backgroundPosition:"center center",
         }}>
             <div className="banner_content">
                 <h1 className="banner_title">Movie Name</h1>
-                <div className="banner_button">
-                    <button>Play</button>
-                    <button>My List</button>
+                <div className="banner_buttons">
+                    <button className='banner_button'>Play</button>
+                    <button className='banner_button'>My List</button>
                 </div>
-                <h1 className="banner_description">This is a test description</h1>
-                h1
+                <h1 className="banner_description">{truncate(`This is a test descriptionhasfhiuafiacicbsacb asuhfuiac dchbiddc zdsiuafuiaz dbcizcbzc asciabcidac zhcizczd Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem nostrum, rerum quaerat, recusandae dolor laboriosam, perferendis tenetur iste modi minima quas earum? Quasi, nihil enim repudiandae animi ipsum consectetur praesentiumb Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit odio, repudiandae quibusdam esse beatae delectus neque officiis omnis facilis eveniet magni enim sapiente praesentium amet eaque illo ad! Est, amet`,250)}</h1>
+                
             </div>
+            <div className="banner_fadeBottom"></div>
         </header>
   )
 }
